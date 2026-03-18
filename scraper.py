6cfa04331435f61f769cf5395607485d01b9479f
JoanneKoshy <joannealicethomas@gmail.com>
2026-03-18 16:12:38 +0530

    changes in the scraper

--- scraper.py
+++ scraper.py
@@ -4,9 +4,8 @@ from bs4 import BeautifulSoup
 def scrape_website(url: str) -> str:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
         "Referer": "https://www.google.com/"
@@ -14,6 +13,7 @@ def scrape_website(url: str) -> str:
 
     response = requests.get(url, headers=headers, timeout=15)
     response.raise_for_status()
+    response.encoding = response.apparent_encoding
 
     soup = BeautifulSoup(response.text, "html.parser")
 
